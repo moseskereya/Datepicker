47391b160c1a3c5ddf9a15d7b9fcfc274b0a40d6
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-21 14:14:12 +0300

    Updates

---
+++ precache-manifest.532e779d6fcf9385c1e7cfacd9ca8f1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382f16ff83e533187fa01a4bddd94a3f",
+    "url": "/Datepicker/index.html"
+  },
+  {
+    "revision": "5c28f5d11e61201536a5",
+    "url": "/Datepicker/static/css/main.19946532.chunk.css"
+  },
+  {
+    "revision": "27256a17baae48e95d08",
+    "url": "/Datepicker/static/js/2.feb9412e.chunk.js"
+  },
+  {
+    "revision": "5c28f5d11e61201536a5",
+    "url": "/Datepicker/static/js/main.0f8d606f.chunk.js"
+  },
+  {
+    "revision": "20afef85de8f5b40a42f",
+    "url": "/Datepicker/static/js/runtime-main.415f822a.js"
+  }
+]);
